25793135271964c71faebb5f94e4dfd1a9fd8828
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-13 20:34:10 +0400

    modified index

--- order1/src/index.js
+++ order1/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import CartProvider from './store/CartProvider';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './route/Login';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <CartProvider>
-    <App />
-  </CartProvider>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/app" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </CartProvider>
 );
