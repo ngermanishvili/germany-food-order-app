dbd2f896c80d0843d52a15d82f2cbfbc29e8267a
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-13 21:13:01 +0400

    chore: made minor changes or improvements that do not affect functionality

--- order1/src/route/Login.js
+++ order1/src/route/Login.js
@@ -1,39 +1,36 @@
-import React from 'react'
-import styles from './Login.module.css'
-import luffy from '../assets/luffy.png'
-
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from './Login.module.css';
+import luffy from '../assets/luffy.png';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/App');
+  };
+
+  const handleGithub = () => {
+    window.open('https://github.com/ngermanishvili', '_blank');
+  }
+
   return (
     <>
       <div className={styles.logo}>
-        <h1>Nika Germanishvili </h1>
+        <h1 onClick={handleGithub}>Nika Germanishvili</h1>
       </div>
-
-
-
-      <div className={styles.container} >
-
+      <div className={styles.container}>
         <div className={styles.luffy}>
-          <img src={luffy} />
-
+          <img src={luffy} alt="Luffy" />
         </div>
-
-
-        <div className={styles.tablet}>
-
-        </div>
-
-        <div className={styles.tablet2} />
-
-        <button className={styles.button}> Login </button>
-
-
+        <div className={styles.tablet}></div>
+        <div className={styles.tablet2}></div>
+        <button className={styles.button} onClick={handleLogin}>
+          Login
+        </button>
       </div>
     </>
+  );
+};
 
-
-  )
-}
-
-export default Login
+export default Login;
