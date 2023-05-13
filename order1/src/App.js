d0c8e3aab8c19fdb463d3154985e80f32dd9e76d
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-13 20:35:21 +0400

    minor changes inside app

--- order1/src/App.js
+++ order1/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
@@ -17,11 +18,11 @@ function App() {
 
   return (
     <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
     </CartProvider>
   );
 }
