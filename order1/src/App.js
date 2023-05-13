20fb08c95fb1e30ec4eb4656bd78737a22ff493a
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-13 18:02:39 +0400

    chore: add minor changes inside app.js for localstorage setup

--- order1/src/App.js
+++ order1/src/App.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
@@ -18,11 +17,11 @@ function App() {
 
   return (
     <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
     </CartProvider>
   );
 }
